f741bbb3e1198f2e1211595091482d98e2399f9e
NikilaNirmala <nikilanirmala1106@gmail.com>
2025-10-02 21:41:35 +0530

    mongodb connection error fix

--- database/migrations/2025_09_22_020044_create_reviews_table.php
+++ database/migrations/2025_09_22_020044_create_reviews_table.php
@@ -1,16 +1,19 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Jenssegers\Mongodb\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    protected $connection = 'mongodb';
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+
+
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->string('title');
